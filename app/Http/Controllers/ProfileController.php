59f0089ba84c0892adb37a1f60bd637fd939358d
Ju He <hejunion@gmail.com>
2021-11-26 11:29:26 -0800

    Init..

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use ILLuminate\Support\Facades\Auth;
+
+use App\Models\Profile;
+
+
+class ProfileController extends Controller
+{
+    //
+    public function index() {
+        $user = Auth::user();
+        $profile = Profile::where('user_id', $user->id)->first();
+
+        // error here. This should be profile instead of profile.index
+        return view( 'profile', [
+            'user' => $user,
+            'profile' => $profile,
+        ] );
+    }
+
+
+
+    public function create(){
+        return view('createProfile');
+    }
+
+    public function postCreate() {
+        $data = request()->validate([
+            'description' => 'required',
+            'profilepic' => ['required', 'image'],
+        ]);
+
+        // store the image in uploads, under public
+        // request(‘profilepic’) is like $_GET[‘profilepic’]
+        $imagePath = request('profilepic')->store('uploads', 'public');
+
+        // create a new profile, and save it
+        $user = Auth::user();
+        $profile = new Profile();
+        $profile->user_id = $user->id;
+        $profile->description = request('description');
+        $profile->image = $imagePath;
+        $saved = $profile->save();
+
+        // if it saved, we send them to the profile page
+        if ($saved) {
+            return redirect('/profile');
+        }
+
+    }
+
+
+}
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+   <div class="row justify-content-center">
+       <div class="col-md-3">
+           <img class="rounded-circle" width="150" src="/storage/{{ $profile->image }}">
+       </div>
+       <div class="col-md-9">
+           <h3>{{ $user->name }}</h3>
+           <span><strong>0</strong> posts</span>
+           <div class="pt-3">{{$profile->description}}</div>
+       </div>
+   </div>
+</div>
+@endsection
